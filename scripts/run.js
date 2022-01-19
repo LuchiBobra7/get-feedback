dac89636f641459862193970ef787a16cd2d6c7d
Luchi Bobra <badger@example.com>
2022-01-19 16:08:21 +0200

    feat: smart contract added

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,32 @@
 const hre = require("hardhat");
 
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const feedbackContractFactory = await hre.ethers.getContractFactory(
     "Feedback"
   );
   const feedbackContract = await feedbackContractFactory.deploy();
   await feedbackContract.deployed();
+  console.log("Contract addy:", feedbackContract.address);
 
-  console.log("Contract deployed to:", feedbackContract.address);
-  console.log("Contract deployed by:", owner.address);
+  let feedbackCount;
+  feedbackCount = await feedbackContract.getTotalFeedbacks();
+  console.log(feedbackCount.toNumber());
 
-  let totalFeedbacks = await feedbackContract.getTotalFeedbacks();
+  let feedbackInfo = await feedbackContract.sendFeedback("A message!", 5);
+  await feedbackInfo.wait();
 
-  console.log("Total Feedbacks:", totalFeedbacks);
+  const [_, randomPerson] = await hre.ethers.getSigners();
+
+  feedbackInfo = await feedbackContract
+    .connect(randomPerson)
+    .sendFeedback("Another message!", 3);
+  await feedbackInfo.wait();
+
+  let feedbackList = await feedbackContract.getFeedbackList();
+  console.log(feedbackList);
+
+  let allFeedbacks = await feedbackContract.getTotalFeedbacks();
+  console.log(allFeedbacks);
 };
 
 const runMain = async () => {
